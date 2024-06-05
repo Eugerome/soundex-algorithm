f9c5751c1f211d96dc4a77f663f2751b8807f8e9
Evgeny <j.romanenkov@gmail.com>
2024-06-05 07:31:05 +0300

    Run from commandline

--- soundex/app.py
+++ soundex/app.py
@@ -4,9 +4,10 @@ import os
 import re
 
 from itertools import groupby
-from typing import Union, Generator
+from typing import Generator, Union
 
 # create logger
+logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 
@@ -79,37 +80,35 @@ class SoundexCode:
                 return "-3"
 
 
-def read_file(file_name: str = "test.txt") -> Union[Generator, None]:
-    """Reads provided file line by line"""
+def get_file_path(file_name: str = "test.txt") -> Union[str, None]:
+    """Returns the full path of the file if it exists in the input folder"""
     # just checking file extensions, file signature is overkill
     supported_extension = ".txt"
     if not file_name.endswith(supported_extension):
-        logging.info(f"Unsupported file '{file_name}', please provide a .txt file")
+        LOGGER.error(f"Unsupported file '{file_name}', please provide a .txt file")
         return
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file_path = f"{dir_path}/input/{file_name}"
 
     if not os.path.isfile(file_path):
-        logging.info(
+        LOGGER.error(
             f"File '{file_name}' not found in the input folder, please make sure you copied it"
         )
         return
+    return file_path
 
-    logging.info(f"Reading file '{file_name}'")
+
+def read_file(file_path: str) -> Generator:
+    """Reads provided file line by line and yields words from it"""
+    LOGGER.debug(f"Reading file '{file_name}'")
     with open(file_path, "r") as file:
         for line in file:
             yield line.split()
 
 
-def run_soundex(target_word: str, file_name: str):
+def run_soundex(target_word: SoundexCode, file_name: str):
     """Runs the soundex code generator"""
-    target_word = SoundexCode(target_word)
-    if not target_word.value:
-        LOGGER.info(
-            "Unsupported character found in word, please use only ascii alphabet letters"
-        )
-        return
     matches = []
     for line in read_file(file_name):
         if not line:
@@ -125,6 +124,17 @@ def run_soundex(target_word: str, file_name: str):
 
 
 if __name__ == "__main__":
-    # a = SoundexCode("Ddimadubizumi")
-    matches = run_soundex("line", "test.txt")
-    print("Hello, World!")
+    file_path = None
+    target_word = None
+    while not file_path:
+        file_name = input("Enter the file name: ")
+        file_path = get_file_path(file_name)
+    while True:
+        target_word = input("Enter the word you want to match: ")
+        target_word = SoundexCode(target_word)
+        if target_word.value:
+            break
+        LOGGER.warning(
+            "Unsupported character found in the word, please use ascii letters only"
+        )
+    matches = run_soundex(target_word, file_path)
